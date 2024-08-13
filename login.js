74021fdc2b0b828d76b287e0ade3cd7a3f7aa713
MysteryMaiden <sageriley09@icloud.com>
2024-08-13 15:30:20 -0400

    Add files via upload

---
+++ login.js
@@ -0,0 +1,27 @@
+let words = new URLSearchParams(window.location.search);
+
+let username = 'SageRH@TheCodingMaster2009$5150';
+let passcode = 'CodeCademy#PortfolioProject@5150!$';
+
+
+
+function isCorrect(userinput, pwinput){
+    if(userinput === username && pwinput === passcode){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function changeTab(blank1, blank2){
+    if(isCorrect(blank1, blank2) === false){
+        alert("The password or username is incorrect, please try again.")
+    } else if (isCorrect(blank1, blank2) === true){
+        window.location.href = 'file:///Users/sage/Desktop/Webpages/Cheet%20Sheet/portfolio%20project/Inbox%20page%20and%20Login/inbox.html';
+    }
+}
+// when submit button is pressed i want it to changeTab
+let password = (words.get('login-pw'))
+let user = (words.get('login-user'))
+changeTab(user, password)
+
